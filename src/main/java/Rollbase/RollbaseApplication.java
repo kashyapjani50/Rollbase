6ffe0a8671094fda30b75c747d49851bbc621167
kashyap jani <kashyapjani50@gmail.com>
2024-12-10 19:06:11 +0530

    Rollbase

--- src/main/java/Rollbase/RollbaseApplication.java
+++ src/main/java/Rollbase/RollbaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RollbaseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RollbaseApplication.class, args);
 	}
 
